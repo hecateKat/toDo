71c5ba6dd24ee6c1303c3a7ab3ab6acbeda3000b
Kat <kat.ochmanska@gmail.com>
2020-06-03 18:24:36 +0200

    added tests after refactoring

--- src/test/java/com/kat/HelloServiceTest.java
+++ src/test/java/com/kat/HelloServiceTest.java
@@ -2,59 +2,70 @@ package com.kat;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 public class HelloServiceTest {
 
-    private HelloService helloService = new HelloService();
+    private final static String WELCOME = "Hello";
 
     @Test
-    public void testShouldReturnTrueWhenMethod_returnsGreeting_returnsWithFallback(){
+    public void testShouldReturnTrueWhenMethod_returnsGreeting_returnsWithFallbackWhenNameIsNull(){
         //given
-        String name = null;
+        var mockRepository = alwaysReturningHelloRepository(WELCOME);
+        var helloService = new HelloService(mockRepository);
 
         //when
-        var result = helloService.prepareGreeting(name);
+        var result = helloService.prepareGreeting(null, "-1");
 
         //then
-        assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void testShouldReturnTrueWhenMethod_returnsGreeting_returnsGreetingWithFallbackIdLang(){
+        //given
+        var fallbackIdWelcome = "Hola";
+        var mockRepository = new LangRepository(){
+            @Override
+            Optional<Lang> findById(Long id) {
+                if (id.equals(HelloService.FALLBACK_LANG.getId())){
+                    return Optional.of(new Lang(null, "Hola", null));
+                }
+                return Optional.empty();
+            }
+        };
+        var helloService = new HelloService(mockRepository);
+
+        //when
+        var result = helloService.prepareGreeting(null, null);
+
+        //then
+        assertEquals(fallbackIdWelcome + " " + HelloService.FALLBACK_NAME + "!", result);
     }
 
     @Test
     public void testShouldReturnTrueWhenMethod_returnsGreeting_returnsWithName(){
         //given
+        var helloService = new HelloService();
         String name = "test";
 
-        //when
-        var result = helloService.prepareGreeting(name);
-
-        //then
-        assertEquals("Hello " + name + "!", result);
-    }
-
-    @Test
-    public void testShouldNotReturnTrueWhenMethod_returnsGreeting_returnsWithFallback(){
-        //given
-        String name = null;
 
         //when
-        var result = helloService.prepareGreeting("hurr");
+        var result = helloService.prepareGreeting(name, "-1");
 
         //then
-        assertNotEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
+        assertEquals(WELCOME + " " + name + "!", result);
     }
 
-    @Test
-    public void testShouldNotReturnTrueWhenMethod_returnsGreeting_returnsWithName(){
-        //given
-        String name = "test";
 
-        //when
-        var result = helloService.prepareGreeting("hurr");
-
-        //then
-        assertNotEquals("Hello " + name + "!", result);
+    private LangRepository alwaysReturningHelloRepository(String WELCOME) {
+        return new LangRepository(){
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.of(new Lang(null, WELCOME, null));
+            }
+        };
     }
-
 }
